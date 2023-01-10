0b6ec205f17e9b9bfb390d78b996196f1916082a
Ashutosh Kumar Azad <ashutoshkumarazad@Ashutoshs-MacBook-Pro.local>
2023-01-11 01:58:54 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import jahaanImg from './jahaan.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h3>Hi ther, I'm JAHAAN</h3>
+     <img src={jahaanImg} width="100%" height="100%" alt="I'm JAHAAN"/>
     </div>
   );
 }
