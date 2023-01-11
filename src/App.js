6d898c73a1ebf897f2685899db7149e161dbbce4
Ashutosh Kumar Azad <ashutoshkumarazad@Ashutoshs-MacBook-Pro.local>
2023-01-11 18:45:28 +0530

    Fixing spelling mistake

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import jahaanImg from './jahaan.png';
 function App() {
   return (
     <div className="App">
-     <h3>Hi ther, I'm JAHAAN</h3>
+     <h3>Hi there, I'm JAHAAN</h3>
      <img src={jahaanImg} width="100%" height="100%" alt="I'm JAHAAN"/>
     </div>
   );
